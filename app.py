2ac041f03d57ae1c400da80f82183e3ef3221976
maxscender <koubaakhaled@gmail.com>
2024-11-15 14:47:01 +0100

    hi2

--- app.py
+++ app.py
@@ -14,5 +14,19 @@ def categorize_odd(value):
     else:
         return 'high'
 df['odd_range'] = df['odd'].apply(categorize_odd)
+
+def categorize_time(dt):
+    hour = pd.to_datetime(dt).hour
+    if 5 <= hour < 12:
+        return 'morning'
+    elif 12 <= hour < 17:
+        return 'afternoon' 
+    elif 17 <= hour < 21:
+        return 'evening'
+    else:
+        return 'night'
+
+df['time_of_day'] = df['dt'].apply(categorize_time)
+
 st.dataframe(df)
 
