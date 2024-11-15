77cb9129878d86edc73abf19d72d74346cba49a3
maxscender <koubaakhaled@gmail.com>
2024-11-15 14:39:22 +0100

    r

--- app.py
+++ app.py
@@ -4,4 +4,15 @@ import streamlit as st
 
 df = pd.read_csv("sofascore.csv")
 
+df['odd'] = pd.to_numeric(df['odd'])
+
+def categorize_odd(value):
+    if value < 1.5:
+        return 'low'
+    elif value < 2:
+        return 'medium'
+    else:
+        return 'high'
+df['odd_range'] = df['odd'].apply(categorize_odd)
 st.dataframe(df)
+
